04989e3bd2a4f083107236226bd5d5dca4194e9e
AgnesVelemi <agnes.kommunikation@gmail.com>
2026-03-05 15:01:35 +0100

    header content, timezone ready

---
+++ src/main/java/dev/siample/dashboard/dto/DashboardStatusDto.java
@@ -0,0 +1,30 @@
+package dev.siample.dashboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class DashboardStatusDto {
+
+    private int connectedClients;
+    private long totalMessages;
+    private final ZonedDateTime serverStartTime = ZonedDateTime.now();
+    private final String serverStartTimeAsString = serverStartTime.format(DateTimeFormatter.ofPattern("yyyy-MMM-dd HH:mm:ss", Locale.ENGLISH));
+    private final String TheTimeZones = serverStartTime.format(DateTimeFormatter.ofPattern("z", Locale.ENGLISH))
+        + " = " + serverStartTime.format(DateTimeFormatter.ofPattern("zzzz", Locale.ENGLISH));
+    private LocalDateTime timestamp;
+
+
+}
